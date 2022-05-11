152d437efab8255cc3a535aebe34c84a97a2bca7
DavidChavarria <campillo-10@hotmail.com.ar>
2022-05-10 20:30:28 -0500

    portafolio con informacion de david

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 from flask import Flask, render_template
+from jinja2 import Undefined
 
 app = Flask(__name__)
+
+inte = Undefined
 
 @app.route("/")
 @app.route("/principal")
 def principal():
     return render_template("base.html")
 
-@app.route("/integrante")
-def integrante():
-    return render_template("integrante.html")
+@app.route("/david")
+def david():
+    return render_template("david.html")
+
+@app.route("/duvan")
+def duvan():
+    return render_template("duvan.html")
 
 app.run(debug=True)
